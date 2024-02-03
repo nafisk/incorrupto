3d4efdf1e6492b01269c5d22045ef6bb0f82e289
nafisk <nafisrizwank@gmail.com>
2024-02-03 13:23:37 -0500

    remove comments

--- server/test/nafis.py
+++ server/test/nafis.py
@@ -3,25 +3,6 @@ from openai import AsyncOpenAI
 import requests
 import json
 
-# client = AsyncOpenAI(
-#     # This is the default and can be omitted
-#     api_key=os.environ.get("OPENAI_API_KEY"),
-# )
-
-
-# response = client.images.generate(
-#   model="dall-e-3",
-#   prompt="a white siamese cat",
-#   size="1024x1024",
-#   quality="standard",
-#   n=1,
-# )
-
-# image_url = response
-
-
-
-
 def generate_image(prompt, api_key):
     headers = {
         'Authorization': f'Bearer {api_key}'
